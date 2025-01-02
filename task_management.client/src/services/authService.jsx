e2a4761ca7f28bd60db4893051e07c0e293bf4fa
Nishant Shingate <nishantks121@gmail.com>
2025-01-02 23:05:26 +0530

    Added Auth Service.

---
+++ task_management.client/src/services/authService.jsx
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const API_URL = 'http://localhost:5182/user';
+
+
+export const registerUser = async (userData) => {
+    try{
+        const response  = await axios.post(`${API_URL}/register`,userData);
+        return response.data;
+    }
+    catch{
+        console.error("Registration Failed!",error);
+        throw error;
+    }
+}
+
+
+export const loginUser = async (loginData) => {
+    try{
+        const response = await axios.post(`${API_URL}/token`,loginData);
+        return response.data;
+    }
+    catch{
+        console.error("Login failed!",error);;
+        throw error;
+    }
+}
