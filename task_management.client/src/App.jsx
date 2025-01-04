01575fec92c795477c94e40c789df247e72caf74
Nishant Shingate <nishantks121@gmail.com>
2025-01-04 23:55:19 +0530

    Refactored Routes

--- task_management.client/src/App.jsx
+++ task_management.client/src/App.jsx
@@ -1,27 +1,51 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import HomePage from "./components/HomePage";
+import HomePage from './components/HomePage';
 import LoginPage from "./components/LoginPage";
 import AboutPage from "./components/AboutPage";
 import EditTaskPage from './components/EditTaskPage';
 import RegistrationPage from './components/RegistrationPage';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { ProtectedRoute, PublicRoute } from './Helper/RouteGuards';
+import NotFoundPage from './components/NotFoundPage'
 
 import NavigationBar from './components/NavigationBar';
 
 function App() {
   return (
     <Router>
-      <NavigationBar />
-      <Routes>
-        <Route path="/" element={<ProtectedRoute> <HomePage /></ProtectedRoute>} />
-        <Route path="/login" element={<PublicRoute><LoginPage /></PublicRoute>} />
-        <Route path='/about' element={<PublicRoute><AboutPage /></PublicRoute>} />
-        <Route path='/edittask' element={<ProtectedRoute><EditTaskPage /></ProtectedRoute>} />
-        <Route path='/registration' element={<PublicRoute><RegistrationPage /></PublicRoute>} />
-      </Routes>
-    </Router>
+          <NavigationBar />
+  <Routes>
+    {/* Protected Routes */}
+    <Route 
+      path="/" 
+      element={
+        <ProtectedRoute>
+          <HomePage />
+        </ProtectedRoute>
+      } 
+    />
+    <Route 
+      path="/edittask" 
+      element={
+        <ProtectedRoute>
+          <EditTaskPage />
+        </ProtectedRoute>
+      } 
+    />
+
+    {/* Public Routes */}
+    <Route path="/login" element={<PublicRoute><LoginPage /></PublicRoute>} />
+    <Route path="/registration" element={<PublicRoute><RegistrationPage /></PublicRoute>} />
+
+    {/* Unrestricted Routes */}
+    <Route path="/about" element={<AboutPage />} />
+
+    {/* Fallback Route */}
+    <Route path="*" element={<NotFoundPage />} />
+  </Routes>
+</Router>
+
   );
 }
 
