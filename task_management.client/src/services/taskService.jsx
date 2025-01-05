9c176d5300c55e0abbdc3024955f1764e875a882
Nishant Shingate <nishantks121@gmail.com>
2025-01-05 12:32:44 +0530

    Added Task Service

--- task_management.client/src/services/taskService.jsx
+++ task_management.client/src/services/taskService.jsx
@@ -1,14 +1,13 @@
 import axios from "axios";
 
 const API_URL = "http://localhost:5182/Task";
-const TOKEN = localStorage.getItem('jwt_token');
 
 export const GetTasks = async () => {
-    //console.log(TOKEN);
+    const TOKEN = localStorage.getItem('jwt_token');
     try {
         const response = await axios.get(`${API_URL}`, {
             headers: {
-                Authorization: `Bearer ${TOKEN}` 
+                Authorization: `Bearer ${TOKEN}`
             }
         });
         return response;
@@ -16,5 +15,27 @@ export const GetTasks = async () => {
     catch (error) {
         console.log("Failed to fetch tasks!", error);
         throw error;
+    }
+}
+
+export const UpdateTask = async (task) => {
+    const TOKEN = localStorage.getItem('jwt_token');
+    try {
+        var ntask = {
+            "id": task.id,
+            "title": task.title,
+            "description": task.description,
+            "dueDate": task.dueDate,
+            "isComplete": task.taskStatus,
+            "priority": task.taskPriority
+        };
+        const response = await axios.put(`${API_URL}`,ntask, {
+            headers: {
+                Authorization: `Bearer ${TOKEN}`
+            },
+        })
+        return response;
+    } catch (error) {
+        console.log("Failed to update task!", error);
     }
 }
